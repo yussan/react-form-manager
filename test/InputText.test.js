af8ce503cefba55c13c4a6b2bec5b24b64af7294
yussan <yussandeveloper@gmail.com>
2017-08-30 16:19:15 +0700

    added test for action

--- test/InputText.test.js
+++ test/InputText.test.js
@@ -1,25 +1,51 @@
 import React from 'react'
-import { shallow, mount, render } from 'enzyme';
-import { expect } from  'chai';
-import sinon from 'sinon';
-import {JSDOM} from 'jsdom';
+import { shallow, mount, render } from 'enzyme'
+import { expect } from  'chai'
+import sinon from 'sinon'
+import {JSDOM} from 'jsdom'
 import {InputText} from '../src/index'
 
 import {setValidation} from '../src/lib/validator/InputValidator'
 
-const dom = new JSDOM('<!doctype html><html><body></body></html>');
-global.document = dom.window.document;
-global.window = dom.window;
+const dom = new JSDOM('<!doctype html><html><body></body></html>')
+global.document = dom.window.document
+global.window = dom.window
 
 describe('<InputText />', () => {
+
   it('allows to set props', () => {
-    const wrapper = mount(<InputText type='text' name='value' value='this is value' />);
-    expect(wrapper.props().value).to.equal('this is value');
-    wrapper.setProps({value:'updated value'});
-    expect(wrapper.props().value).to.equal('updated value');
+    const wrapper = mount(<InputText type='text' name='value' value='this is value' />)
+    expect(wrapper.props().value).to.equal('this is value')
+    wrapper.setProps({value:'updated value'})
+    expect(wrapper.props().value).to.equal('updated value')
   })
 
-  it('on change', ()=> {
-    
+  it('on click', ()=> {
+    const handleClick = sinon.spy()
+    const wrapper = mount(
+      <InputText setState={(obj) => handleClick(obj)} type='text' name='value' value='value' />
+    )
+    wrapper.find('input').simulate('click')
+    // expect.handleClick
+    // console.log(handleClick.args)
+    // expect(handleClick).to.have.property('args', 
+    // [
+    //   [{
+    //     "value": "value",
+    //     "value_validation": {
+    //       "is_valid": true
+    //     }
+    //   }]
+    // ]);
+  })
+
+  it('on valid change', ()=> {
+    const onClick = sinon.spy()
+
+  })
+
+  it('on invalid change', ()=> {
+    const onClick = sinon.spy()
+
   })
 })
